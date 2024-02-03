94038473998d483efd8aa12d41cc45beb179141e
mohammed-ashrf <greenbile2002@gmail.com>
2024-02-03 16:01:46 +0200

    adding UUID

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -2,6 +2,8 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
 import { Task } from '../../Task';
+import { v4 as uuidv4 } from 'uuid';
+
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
@@ -25,6 +27,7 @@ export class AddTaskComponent {
     }
 
     const newTask = {
+      id: uuidv4(),
       text: this.text,
       day: this.day,
       reminder: this.reminder
